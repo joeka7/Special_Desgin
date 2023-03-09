3b13a9db87b9f45a744633a19b86f374b9198fe7
Joe-ka <ykaram599@gmail.com>
2023-03-09 18:19:11 +0200

    Update

--- js/master.js
+++ js/master.js
@@ -20,24 +20,45 @@ window.onscroll = function() {
 // Create Popup With The Image
 let ourGallery = document.querySelectorAll(".gallery img");
 ourGallery.forEach(img => {
-    img.addEventListener("click", (e) => {
-        // Create Overlay Element
+    img.addEventListener("click", () => {
+        // Create Overlay
         let overlay = document.createElement("div");
-        // Add Class To Overlay
         overlay.className = "popup-overlay";
-        // Apend Overlay To The Body
         document.body.appendChild(overlay);
         // Create The Popup
         let popupBox = document.createElement("div");
-        // Add Class to The Popup Box
         popupBox.className = "popup-box";
+        // if There's alt In Image
+        if (img.alt !== null) {
+            // Create Heading
+            let imgHeading = document.createElement("h3");
+            // Create Text For Heading
+            let imgText = document.createTextNode(img.alt);
+            // Append The Text To The Heading
+            imgHeading.appendChild(imgText);
+            // Append The Heading To The Popup Box
+            popupBox.appendChild(imgHeading);
+        };
         // Create The Image
         let popupImage = document.createElement("img");
-        // Set Image Source
         popupImage.src = img.src;
-        // Add Image To Popup Box
         popupBox.appendChild(popupImage);
         // Append The Popup Box To Body
         document.body.appendChild(popupBox);
+        // Create Close Span
+        let colseButton = document.createElement("span");
+        colseButton.className = "close-button";
+        let closeText = document.createTextNode("X");
+        colseButton.appendChild(closeText);
+        popupBox.appendChild(colseButton);
     });
+});
+
+// Close Popup In Image
+document.addEventListener("click", (e) => {
+    if (e.target.className === "close-button") {
+        // Remove The Current Popup
+        e.target.parentNode.remove();
+        document.querySelector(".popup-overlay").remove();
+    };
 });
